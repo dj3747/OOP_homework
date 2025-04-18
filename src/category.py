54428515233bbf1d5abd6414c206c9f3119ec62c
dj3747 <dj94911131@gmail.com>
2025-04-18 21:40:54 +0300

    fixed product.py and category.py

--- src/category.py
+++ src/category.py
@@ -1,15 +1,35 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
-    products: list
+    __products: list # Приватный атрибут
     category_count = 0
     product_count = 0
 
     def __init__(self, name: str, description: str, products: list) -> None:
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         # Увеличение значения атрибутов класса
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product: Product) -> None:
+        if not isinstance(product, Product):
+            raise TypeError("Можно добавлять только объекты класса Product или его наследников")
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def product(self) -> str:
+        return "\n".join(
+                [f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт." for p in self.__products]
+    )
+
+
+    def get_products(self) -> list:
+        return self.__products
+
--- src/product.py
+++ src/product.py
@@ -1,11 +1,42 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float # Приватный атрибут
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+@classmethod
+def new_product(cls, product_dict: dict, existing_products: list):
+    """Создаёт новый товар или обновляет существующий"""
+    for product in existing_products:
+        if product.name == product_dict["name"]:
+            # Обновляем количество товара и выбираем более высокую цену
+            product.quantity += product_dict["quantity"]
+            product.price = max(product.price, max(0, product_dict["price"])) # Учитываем отрицательную цену
+            return product
+
+        # Создаем новый товар, если такого еще нет
+        return cls(**product_dict) # Используем ** для распаковки словаря
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif value < self.__price:
+            confirm = input(f"Цена понижается с {self.__price} до {value}. Вы уверены? (y/n): ")
+            if confirm.lower() == "y":
+                self.__price = value
+                print(f"Цена изменена на {value}")
+            else:
+                print("Изменение цены отменено")
+        else:
+            self.__price = value
