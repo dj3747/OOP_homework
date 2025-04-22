29c63d5a5a14a7d8b5b10071890e7fbe5a1c7030
dj3747 <dj94911131@gmail.com>
2025-04-22 15:12:45 +0300

    fixed product.py

--- src/product.py
+++ src/product.py
@@ -10,6 +10,14 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        if not isinstance(other, Product):
+            raise TypeError("Можно складывать только объекты класса Product")
+        return self.price * self.quantity + other.price * other.quantity
+
     @classmethod
     def new_product(cls, product_dict: dict, existing_products: list):
         """Создаёт новый товар или обновляет существующий"""
